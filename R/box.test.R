00eb041971b92468ff56a01dd84796832e5bb46b
Osman Dag <osman.dag@outlook.com>
2023-01-18 14:00:02 +0000

    version 2.7

--- R/box.test.R
+++ R/box.test.R
@@ -1,88 +1,82 @@
 box.test <- function(formula, data, alpha = 0.05, na.rm = TRUE, verbose = TRUE) {
-
+  
   data <- model.frame(formula, data)
   dp <- as.character(formula)
   DNAME <- paste(dp[[2L]], "and", dp[[3L]])
-
+  
   METHOD <- "Box F Test"
-
-
- if (na.rm){
+  
+  
+  if (na.rm){
     completeObs <- complete.cases(data)
     data <- data[completeObs,]
   }
-
-if (any(colnames(data)==dp[[3L]])==FALSE) stop("The name of group variable does not match the variable names in the data. The group variable must be one factor.")
-if (any(colnames(data)==dp[[2L]])==FALSE) stop("The name of response variable does not match the variable names in the data.")
-
-y = data[[dp[[2L]]]]
-group = data[[dp[[3L]]]]
-
-
-if (!(is.factor(group)|is.character(group))) stop("The group variable must be a factor or a character.") 
-if (is.character(group)) group <- as.factor(group)
-if (!is.numeric(y)) stop("The response must be a numeric variable.") 
-
-n <- length(y)
-x.levels <- levels(factor(group))
-k <- length(x.levels)
-y.means <- y.n <- y.vars <- NULL
-
-
-for (i in x.levels) {
-y.means[i] <- mean(y[group==i])
-y.n[i] <- length(y[group==i])
-y.vars[i] <- var(y[group==i])
-}
-
-
-f <- y.n/length(y)
-
-df1 <- (sum((1 - f) * y.vars))^2/((sum(y.vars * f))^2 + sum((y.vars^2) * (1 - 2 * f)))
-
-df2 <- ((sum((1 - f) * y.vars))^2)/((sum(y.vars^2 * (1 - f)^2))/sum(y.n - 1))
-
-Ftest <- (sum(y.n * ((y.means - mean(y))^2)))/(sum((1 - (y.n/length(y))) * y.vars))
-
-
-p.value=pf(Ftest,df1,df2,lower.tail = F)
-
   
+  if (any(colnames(data)==dp[[3L]])==FALSE) stop("The name of group variable does not match the variable names in the data. The group variable must be one factor.")
+  if (any(colnames(data)==dp[[2L]])==FALSE) stop("The name of response variable does not match the variable names in the data.")
+  
+  y = data[[dp[[2L]]]]
+  group = data[[dp[[3L]]]]
+  
+  
+  if (!(is.factor(group)|is.character(group))) stop("The group variable must be a factor or a character.") 
+  if (is.character(group)) group <- as.factor(group)
+  if (!is.numeric(y)) stop("The response must be a numeric variable.") 
+  
+  n <- length(y)
+  x.levels <- levels(factor(group))
+  k <- length(x.levels)
+  y.means <- tapply(y, group, mean)
+  y.n <- tapply(y, group, length)
+  y.vars <- tapply(y, group, var)
+  
+  f <- y.n/length(y)
+  
+  df1 <- (sum((1 - f) * y.vars))^2/((sum(y.vars * f))^2 + sum((y.vars^2) * (1 - 2 * f)))
+  
+  df2 <- sum((y.n - 1)*y.vars)^2/sum((y.n-1)*y.vars^2)
 
-
-if (verbose) {
-            cat("\n", "",METHOD,paste("(alpha = ",alpha,")",sep = ""), "\n", 
-                sep = " ")
-            cat("-------------------------------------------------------------", 
-                "\n", sep = " ")
-            cat("  data :", DNAME, "\n\n", sep = " ")
-            cat("  statistic  :", Ftest, "\n", sep = " ")
-            cat("  num df     :", df1, "\n", sep = " ")
-		 cat("  denom df   :", df2, "\n", sep = " ")
-		 cat("  p.value    :", p.value, "\n\n", sep = " ")
-            cat(if (p.value > alpha) {
-                "  Result     : Difference is not statistically significant."
-            }
-            else {
-                "  Result     : Difference is statistically significant."
-            }, "\n")
-            cat("-------------------------------------------------------------", 
-                "\n\n", sep = " ")
-        }
-
-result <- list()
-result$statistic <- Ftest
-result$parameter <- c(df1,df2)
-result$p.value <- p.value
-result$alpha <- alpha
-result$method <- METHOD 
-result$data <- data
-result$formula <- formula
-
-attr(result, "class") <- "owt"
-invisible(result)
-
-
+  Ftest <- (sum(y.n * ((y.means - mean(y))^2)))/(sum((1 - (y.n/length(y))) * y.vars))
+  
+  
+  p.value=pf(Ftest,df1,df2,lower.tail = F)
+  
+  
+  
+  
+  if (verbose) {
+    cat("\n", "",METHOD,paste("(alpha = ",alpha,")",sep = ""), "\n", 
+        sep = " ")
+    cat("-------------------------------------------------------------", 
+        "\n", sep = " ")
+    cat("  data :", DNAME, "\n\n", sep = " ")
+    cat("  statistic  :", Ftest, "\n", sep = " ")
+    cat("  num df     :", df1, "\n", sep = " ")
+    cat("  denom df   :", df2, "\n", sep = " ")
+    cat("  p.value    :", p.value, "\n\n", sep = " ")
+    cat(if (p.value > alpha) {
+      "  Result     : Difference is not statistically significant."
+    }
+    else {
+      "  Result     : Difference is statistically significant."
+    }, "\n")
+    cat("-------------------------------------------------------------", 
+        "\n\n", sep = " ")
+  }
+  
+  result <- list()
+  result$statistic <- Ftest
+  result$parameter <- c(df1,df2)
+  result$p.value <- p.value
+  result$alpha <- alpha
+  result$method <- METHOD 
+  result$data <- data
+  result$formula <- formula
+  
+  attr(result, "class") <- "owt"
+  invisible(result)
+  
+  
 }
 
 
